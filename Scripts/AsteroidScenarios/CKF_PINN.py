Fixed units for PINN gravity filter

Scripts/AsteroidScenarios/CKF_PINN.py
@@ -20,8 +20,9 @@ import GravNN
 
 def main():
     ep = ErosParams()
-    cart_state = np.array([2.40000000e+04, 0.0, 0.0, 
-                           0.0, 4.70033081e+00, 4.71606150e-01]) + ep.X_BE_E
+    X_SB_E = np.array([2.40000000e+04, 0.0, 0.0, 
+                           0.0, 4.70033081e+00, 4.71606150e-01]) / 1E3 # SC to Asteroid in km
+    cart_state = X_SB_E + ep.X_BE_E
                            
     model = pinnGravityModel(os.path.dirname(GravNN.__file__) + \
         "/../Data/Dataframes/eros_BVP_PINN_III.data")
@@ -43,7 +44,7 @@ def main():
     t0 = 0.0
 
     # Initialize Process Noise
-    Q0 = np.eye(3) * 1e-6 ** 2
+    Q0 = np.eye(3) * 1e-7 ** 2
     Q_args = []
     Q_fcn = process_noise(x0, Q0, get_Q, Q_args, use_numba=False)
 
@@ -81,6 +82,8 @@ def main():
     logger = FilterLogger(len(x0), len(t))
     filter = KalmanFilter(t0, x0, dx0, P0, f_dict, h_dict, logger=logger)
     filter.f_integrate = dynamics_ivp_no_jit # can't pass the model into the numba JIT function
+    filter.atol = 1E-7
+    filter.rtol = 1E-7
     filter.run(t, Y[:,1:], R_vec, f_args_vec, h_args_vec)
     print("Time Elapsed: " + str(time.time() - start_time))
 

Scripts/Trajectories/generate_asteroid_trajectory.py
@@ -18,9 +18,9 @@ def generate_asteroid_trajectory():
     eros = Eros()
     gravity_model = Polyhedral(eros, eros.obj_8k)
 
-    # Generate State
+    # Generate State in km and km/s
     X0 = np.array([2.40000000e+04, 0.0, 0.0, 
-                   0.0, 4.70033081e+00, 4.71606150e-01])/1E3
+                   0.0, 4.70033081e+00, 4.71606150e-01])/1E3 
     N = len(X0)
 
     pbar = ProgressBar(t_f, enable=True)
@@ -36,7 +36,7 @@ def generate_asteroid_trajectory():
 
     data = {
         "t" : sol.t,
-        "X" : sol.y[:N, :].T,
+        "X" : sol.y[:N, :].T, # in km and km/s
     }
     with open('Data/Trajectories/trajectory_asteroid.data', 'wb') as f:
         pickle.dump(data, f)

StatOD/constants.py
@@ -27,4 +27,4 @@ class ErosParams:
         G = 6.67430 * 10 ** -11
         self.mu = G * 6.687 * 10 ** 15 / (1E3)**3
         self.R = 16.000 # km
-        self.X_BE_E = np.array([1.995636E7,0,0,0,0,0]) # Assume Eros is 0.1334 AU from Earth
+        self.X_BE_E = np.array([1.995636E7,0,0,0,0,0]) # [km, km/s] Assume Eros is 0.1334 AU from Earth