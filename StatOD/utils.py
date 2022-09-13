be18f134469bf1d263c27c156cff12e4d291ff41
John M <john.r.martin@colorado.edu>
2022-09-13 16:49:03 -0600

    Code for non-dimensionalization scripts

--- StatOD/utils.py
+++ StatOD/utils.py
@@ -121,7 +121,7 @@ import pandas as pd
 import tensorflow as tf
 
 class pinnGravityModel():
-    def __init__(self, df_file, custom_data_dir="", learning_rate=None):
+    def __init__(self, df_file, custom_data_dir="", learning_rate=None, dim_constants=None):
         df = pd.read_pickle(custom_data_dir + df_file)
         config, gravity_model = load_config_and_model(df.iloc[-1]['id'], df)
         self.config = config
@@ -130,6 +130,15 @@ class pinnGravityModel():
         if learning_rate is not None:
             self.config['learning_rate'][0] = learning_rate
         self.optimizer = configure_optimizer(self.config, None)
+        if dim_constants is None:
+            self.dim_constants = {
+                "m_star" : 1.0,
+                "t_star" : 1.0,
+                "l_star" : 1.0,
+            }
+        else:
+            self.dim_constants = dim_constants
+
         removed_pm = config.get('remove_point_mass', [False])[0]
         deg_removed = config.get('deg_removed', [-1])[0]
         if removed_pm or deg_removed > -1:
@@ -154,36 +163,45 @@ class pinnGravityModel():
         self.gravity_model.a_postprocessor = a_postprocessor
 
     def generate_acceleration(self, X):
-        R = np.array(X).reshape((-1,3)).astype(np.float32)
+        X_dim = X*self.dim_constants['l_star']
+        R = np.array(X_dim).reshape((-1,3)).astype(np.float32)
         a_model = self.gravity_model.generate_acceleration(R).numpy() # this is currently a_r > 0
-        # a_model *= -1 # to make dynamics work, this will be multiplied by -1    
-        
+
         if not self.removed_pm:
-            return a_model.squeeze()
+            a_model = a_model.squeeze()
         else:
             r = np.linalg.norm(R, axis=1)
             a_pm_sph = np.zeros((len(R), 3))
             a_pm_sph[:,0] = -self.planet.mu/r**2
             r_sph = cart2sph(R)
             a_pm_xyz = invert_projection(r_sph, a_pm_sph)
-            return (a_pm_xyz + a_model).squeeze() # a_r < 0
+            a_model = (a_pm_xyz + a_model).squeeze() # a_r < 0
+
+        a_non_dim = a_model / (self.dim_constants['l_star'] / self.dim_constants['t_star']**2)
+        return a_non_dim
 
 
     def generate_dadx(self, X):
-        R = np.array(X).reshape((-1,3)).astype(np.float32)
-        dadx = self.gravity_model.generate_dU_dxdx(R).numpy() # this is also > 0
-        return dadx.squeeze()
+        X_dim = X*self.dim_constants['l_star']
+        R = np.array(X_dim).reshape((-1,3)).astype(np.float32)
+        dadx_dim = self.gravity_model.generate_dU_dxdx(R).numpy() # this is also > 0
+        dadx_non_dim = dadx_dim / (1.0 / self.dim_constants['t_star']**2)
+        return dadx_non_dim.squeeze()
 
     def generate_potential(self, X):
-        R = np.array(X).reshape((-1,3)).astype(np.float32)
+        X_dim = X*self.dim_constants['l_star']
+        R = np.array(X_dim).reshape((-1,3)).astype(np.float32)
         U_model = self.gravity_model.generate_potential(R).numpy() # this is also > 0
         if not self.removed_pm:
-            return U_model.squeeze()
+            U_model = U_model.squeeze()
         else:
             r = np.linalg.norm(R, axis=1)
             U_pm = np.zeros((len(R), 1))
             U_pm[:,0] = -self.planet.mu/r
-            return (U_pm + U_model).squeeze()
+            U_model = (U_pm + U_model).squeeze()
+
+        U_model_non_dim = U_model / (self.dim_constants['l_star']**2/ self.dim_constants['t_star']**2)
+        return U_model_non_dim
 
     def set_PINN_training_fcn(self, PINN_constraint_fcn):
         PINN_variables = _get_PI_constraint(PINN_constraint_fcn)
@@ -194,12 +212,11 @@ class pinnGravityModel():
 
     def train(self, X, Y, **kwargs):
         # tf.config.run_functions_eagerly(True)
-
-        X_m = X*1E3
-        A_DMC = Y*1E3 # convert to m/s^2
-        A = A_DMC + self.generate_acceleration(X_m)
-        X_process = self.gravity_model.x_preprocessor(X_m).numpy()
-        Y_process = self.gravity_model.a_preprocessor(A).numpy()
+        A = Y + self.generate_acceleration(X)
+        X_dim = X*(self.dim_constants['l_star'])
+        A_dim = A*(self.dim_constants['l_star'] / self.dim_constants['t_star']**2)
+        X_process = self.gravity_model.x_preprocessor(X_dim).numpy()
+        Y_process = self.gravity_model.a_preprocessor(A_dim).numpy()
         if self.config['PINN_constraint_fcn'][0] == "pinn_alc":
             Y_LC = np.full((len(Y_process), 4), 0.0)
             Y_process = np.hstack((Y_process, Y_LC))
