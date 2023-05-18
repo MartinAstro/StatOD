c3fe50e5ee410cb8816a21763234f5c27190a91e
John M <john.r.martin@colorado.edu>
2023-05-17 19:47:05 -0600

    Update rotation scenario

--- Scripts/Experiments/EKF_PINN_DMC_pos_rotating.py
+++ Scripts/Experiments/EKF_PINN_DMC_pos_rotating.py
@@ -1,11 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from GravNN.Analysis.ExtrapolationExperiment import ExtrapolationExperiment
 from GravNN.GravityModels.HeterogeneousPoly import get_hetero_poly_data
-from Scripts.Scenarios.helper_functions import *
+from GravNN.Visualization.ExtrapolationVisualizer import ExtrapolationVisualizer
 
-from Scripts.Visualization.FilterVisualizer import FilterVisualizer
+from Scripts.Analysis.AnalysisBaseClass import AnalysisBaseClass
+from Scripts.Scenarios.helper_functions import *
 from Scripts.Scenarios.helper_functions import get_trajectory_data
 from Scripts.Scenarios.ScenarioPositions import ScenarioPositions
+from Scripts.Visualization.FilterVisualizer import FilterVisualizer
+from Scripts.Visualization.GravityPlanesVisualizer import GravityPlanesVisualizer
 from StatOD.constants import ErosParams
 from StatOD.data import get_measurements_general
 from StatOD.dynamics import *
@@ -13,8 +17,6 @@ from StatOD.filters import ExtendedKalmanFilter
 from StatOD.measurements import h_pos
 from StatOD.models import pinnGravityModel
 from StatOD.visualizations import *
-from Scripts.Analysis.AnalysisBaseClass import AnalysisBaseClass
-from Scripts.Visualization.GravityPlanesVisualizer import GravityPlanesVisualizer
 
 plt.switch_backend("WebAgg")
 
@@ -40,7 +42,7 @@ def EKF_Rotating_Scenario(pinn_file, traj_file, hparams, show=False):
     pinn_constraint_fcn = hparams.get("train_fcn", ["pinn_a"])[0]
     bc_data = hparams.get("boundary_condition_data", [False])[0]
     measurement_noise = hparams.get("measurement_noise", ["noiseless"])[0]
-
+    eager = hparams.get("eager", [False])[0]
     dim_constants = {"t_star": 1e4, "m_star": 1e0, "l_star": 1e1}
 
     # load trajectory data and initialize state, covariance
@@ -64,8 +66,9 @@ def EKF_Rotating_Scenario(pinn_file, traj_file, hparams, show=False):
     t_vec, Y_vec, h_args_vec = get_measurements_general(
         measurement_file,
         t_gap=60,
-        data_fraction=0.1,
+        data_fraction=1.0,  # 0.001,
     )
+    # t_vec[1] = 0
 
     R_diag = np.eye(3) * r**2
 
@@ -79,6 +82,7 @@ def EKF_Rotating_Scenario(pinn_file, traj_file, hparams, show=False):
         pinn_file,
         learning_rate=lr,
         dim_constants=dim_constants_pinn,
+        eager=eager,
     )
     model.set_PINN_training_fcn(pinn_constraint_fcn)
 
@@ -120,6 +124,7 @@ def EKF_Rotating_Scenario(pinn_file, traj_file, hparams, show=False):
     scenario.filter.atol = 1e-10
     scenario.filter.rtol = 1e-10
 
+    new_COM = Eros().radius / 3 * 2 * (1 / 10.0)
     network_train_config = {
         "batch_size": batch_size,
         "epochs": epochs,
@@ -128,6 +133,10 @@ def EKF_Rotating_Scenario(pinn_file, traj_file, hparams, show=False):
         "rotating_fcn": rotating_fcn,
         "synthetic_data": False,
         "num_samples": 1000,
+        "X_COM": np.array([[new_COM, 0.0, 0.0]]),
+        # "X_COM": np.array([[10.0, 0.0, 0.0]]),
+        "COM_samples": 1,
+        "COM_radius": 1e-12,
     }
     scenario.run(network_train_config)
     scenario.dimensionalize()
@@ -152,20 +161,34 @@ def EKF_Rotating_Scenario(pinn_file, traj_file, hparams, show=False):
     analysis.true_gravity_fcn = get_hetero_poly_data
     metrics = analysis.run()
 
-    if show:
-        # Plot experiment results
-        planes_vis = GravityPlanesVisualizer()
-        planes_vis.run(
-            analysis.planes_exp,
-            max_error=10,
-            logger=vis.scenario.filter.logger,
-        )
-        plt.show()
-
-    data_dir = os.path.dirname(StatOD.__file__) + "/../Data"
+    os.path.dirname(StatOD.__file__) + "/../Data"
     hparams.update(metrics)
     model.config.update({"hparams": [hparams]})
-    model.save(None, data_dir)  # save the network, but not into the directory right now
+    network_dir = model.save()  # save the network, but not into the directory right now
+
+    # save the figures into the network directory
+
+    # Plot experiment results
+    planes_vis = GravityPlanesVisualizer(analysis.planes_exp)
+    planes_vis.run(
+        max_error=10,
+        logger=vis.scenario.filter.logger,
+    )
+    planes_vis.save_all(network_dir)
+
+    extrap_exp = ExtrapolationExperiment(model.gravity_model, model.config, points=1000)
+    extrap_exp.config["gravity_data_fcn"] = [get_hetero_poly_data]
+    extrap_exp.run()
+
+    extrap_vis = ExtrapolationVisualizer(extrap_exp)
+    extrap_vis.plot_interpolation_percent_error()
+    extrap_vis.save(plt.gcf(), network_dir + "/extrap_interpolation.png")
+
+    extrap_vis.plot_extrapolation_percent_error()
+    extrap_vis.save(plt.gcf(), network_dir + "/extrap_extrapolation.png")
+
+    if show:
+        plt.show()
 
     # add planes experiment and record metrics into dataframe for parallel coordinate plot (plotly)
     return model.config
@@ -186,12 +209,13 @@ if __name__ == "__main__":
     hparams = {
         "q_value": [1e-9],
         "r_value": [1e-12],
-        "epochs": [100],
+        "epochs": [1000],
         "learning_rate": [1e-4],
-        "batch_size": [1024],
+        "batch_size": [20000],
         "train_fcn": ["pinn_a"],
         "boundary_condition_data": [False],
         "measurement_noise": ["noiseless"],
+        "eager": [False],
     }
 
-    EKF_Rotating_Scenario(pinn_file, traj_file, hparams, show=False)
+    EKF_Rotating_Scenario(pinn_file, traj_file, hparams, show=True)
