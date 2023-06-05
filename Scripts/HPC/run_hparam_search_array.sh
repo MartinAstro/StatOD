a9cabc4c7945a9d02c1212a64609f3c895c36cdd
John M <john.r.martin@colorado.edu>
2023-06-05 17:59:31 -0600

    Slurm array job queue

---
+++ Scripts/HPC/run_hparam_search_array.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --array=0-648
+#SBATCH --mem-per-cpu=2500M
+#SBATCH --time=00:15:00
+#SBATCH --partition=amilan
+#SBATCH --output=SlurmFiles/hparam-search-%j.out
+
+module purge
+
+echo "== Load Anaconda =="
+
+module load anaconda
+
+echo "== Activate Env =="
+
+conda activate research
+
+# Installed package in the compile node already
+# echo "== Install =="
+# cd /projects/joma5012/GravNN/
+# conda develop .
+
+echo "== Run data generation =="
+
+# Note the --exclusive flag ensures that only the one script can be run on the node at a time. 
+echo "Running hparams"
+srun python /projects/joma5012/StatOD/Scripts/HparamsSearch/hparamsSearchArray.py $SLURM_ARRAY_TASK_ID
+
+wait # Necessary to wait for all processes to finish
+echo "== End of Job =="
+exit 0
---
+++ Scripts/HparamsSearch/hparamsSearchArray.py
@@ -0,0 +1,58 @@
+import os
+import sys
+
+from GravNN.Support.slurm_utils import get_available_cores
+
+import StatOD
+from Scripts.Scenarios.DMC_high_fidelity import DMC_high_fidelity
+from StatOD.utils import *
+from StatOD.utils import dict_values_to_list
+
+
+def run_catch(args):
+    os.path.dirname(StatOD.__file__)
+
+    args_list = dict_values_to_list(args)
+
+    if args_list["pinn_file"][0] == "pm":
+        pinn_file = "Data/Dataframes/eros_pm_053123.data"
+        traj_file = "traj_eros_pm_053123"
+    else:
+        pinn_file = "Data/Dataframes/eros_poly_053123.data"
+        traj_file = "traj_eros_poly_053123"
+
+    config = DMC_high_fidelity(
+        pinn_file,
+        traj_file,
+        args_list,
+        show=False,
+    )
+
+    return config
+
+
+def main():
+    hparams = {
+        "q_value": [1e-9, 1e-8, 1e-7],
+        "epochs": [10, 100, 1000],
+        "learning_rate": [1e-4, 1e-5, 1e-6],
+        "batch_size": [256, 2048, 2**15],
+        "train_fcn": ["pinn_a", "pinn_al"],
+        "data_fraction": [0.3],
+        "measurement_noise": ["noiseless", "noisy"],
+        "pinn_file": ["pm", "poly"],
+        "r_value": [1e-12, 1e-3],
+        # "filter_type" : ["CKF", "EKF", "UKF"],
+    }
+
+    # append lock to the list of arg tupl
+    get_available_cores()
+    args = format_args(hparams)
+
+    idx = sys.argv[1]
+    args_i = args[idx]
+    run_catch(args_i)
+
+
+if __name__ == "__main__":
+    main()
