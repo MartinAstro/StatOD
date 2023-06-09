28f758021fd2544856703b2e6a75bfe64493aa86
John M <john.r.martin@colorado.edu>
2023-06-09 16:48:04 -0600

    Compute metrics for standard models

---
+++ Scripts/Tables/compute_metrics.py
@@ -0,0 +1,117 @@
+import os
+
+import pandas as pd
+from GravNN.CelestialBodies.Asteroids import Eros
+from GravNN.GravityModels.HeterogeneousPoly import (
+    # generate_heterogeneous_sym_model,
+    get_hetero_poly_symmetric_data,
+)
+from GravNN.GravityModels.PointMass import PointMass
+from GravNN.GravityModels.Polyhedral import Polyhedral
+from GravNN.GravityModels.SphericalHarmonics import SphericalHarmonics
+from GravNN.Networks.Model import load_config_and_model
+
+import StatOD
+from Scripts.Factories.CallbackFactory import CallbackFactory
+
+
+def compute_metrics(model):
+    callbacks = CallbackFactory().generate_callbacks(pbar=True)
+
+    # run the callbacks
+    t_i_dummy = 0.0  # dummy
+    for callback in callbacks.values():
+        callback(model, t_i_dummy)
+
+    # extract the data from the callbacks
+    metrics = {}
+    for name, callback in callbacks.items():
+        metrics[name] = callback.data[-1]
+
+    # compute the metrics for the table
+    all_metrics = {}
+
+    dX_sum = 0
+    t_sum = 0
+    traj_count = 0
+    for key, value in metrics["Trajectory"].items():
+        if "dX_sum" in key:
+            dX_sum += value
+            traj_count += 1
+        elif "t_" in key:
+            t_sum += value
+
+    all_metrics.update(metrics["Extrapolation"])
+    all_metrics.update(metrics["Planes"])
+    all_metrics.update({"dX_sum": dX_sum, "t_sum": t_sum})
+    all_metrics.update({"Model": model.__class__.__name__})
+
+    # return the metrics
+    return all_metrics
+
+
+def main():
+    metrics_list = []
+    planet = Eros()
+
+    statOD_dir = os.path.dirname(StatOD.__file__)
+    pinn_file = f"{statOD_dir}/../Data/Dataframes/eros_constant_poly.data"
+
+    df = pd.read_pickle(pinn_file)
+
+    config, model = load_config_and_model(
+        df.id.values[-1],
+        df,
+        custom_dtype="float32",
+        only_weights=True,
+    )
+    config.update(
+        {
+            "gravity_data_fcn": [get_hetero_poly_symmetric_data],
+        },
+    )
+
+    ############################################
+    # Plot the True Model
+    ############################################
+    # model = generate_heterogeneous_sym_model(planet, planet.obj_8k)
+    # model.config = config
+    # metrics = compute_metrics(model)
+    # metrics_list.append(metrics)
+
+    ############################################
+    # Plot the Point Mass Model
+    ############################################
+
+    model = PointMass(planet)
+    model.config = config
+    metrics = compute_metrics(model)
+    metrics_list.append(metrics)
+
+    ############################################
+    # Plot the Spherical Harmonics Model
+    ############################################
+
+    regress_deg = 8
+    file_name = f"{statOD_dir}/../Data/Products/SH_Eros_model_{regress_deg}.csv"
+    model = SphericalHarmonics(file_name, regress_deg)
+    model.config = config
+    metrics = compute_metrics(model)
+    metrics_list.append(metrics)
+
+    ############################################
+    # Plot the Polyhedral Model
+    ############################################
+
+    model = Polyhedral(planet, planet.obj_8k)
+    model.config = config
+    metrics = compute_metrics(model)
+    metrics_list.append(metrics)
+
+    # convert the metrics into a dataframe and save to file
+    metrics_df = pd.DataFrame(metrics_list)
+    metrics_df.to_pickle(f"{statOD_dir}/../Data/Dataframes/standard_model_metrics.data")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Scripts/Tables/metrics_table.py
@@ -0,0 +1,88 @@
+import os
+
+import numpy as np
+import pandas as pd
+
+import StatOD
+
+
+def main():
+    # load in metrics dataframe
+    statOD_dir = os.path.dirname(StatOD.__file__)
+    metrics_file = f"{statOD_dir}/../Data/Dataframes/standard_model_metrics.data"
+    metrics_df = pd.read_pickle(metrics_file)
+
+    # replace values in percent_error_avg, percent_error_std, and percent_error_max with
+    # nans if they are greater than 1000
+    metrics_df.loc[metrics_df["percent_error_avg"] > 1000, "percent_error_avg"] = np.nan
+    metrics_df.loc[metrics_df["percent_error_std"] > 1000, "percent_error_std"] = np.nan
+    metrics_df.loc[metrics_df["percent_error_max"] > 1000, "percent_error_max"] = np.nan
+
+    # Format columns to scientific notation
+    metrics_df["dX_sum"] = metrics_df["dX_sum"].map("{:.2e}".format)
+    metrics_df["percent_error_avg"] = metrics_df["percent_error_avg"].map(
+        "{:.2f}".format,
+    )
+    metrics_df["percent_error_std"] = metrics_df["percent_error_std"].map(
+        "{:.2f}".format,
+    )
+    metrics_df["percent_error_max"] = metrics_df["percent_error_max"].map(
+        "{:.2f}".format,
+    )
+
+    # Make a single column for the planes
+    metrics_df["new_col"] = metrics_df.apply(
+        lambda row: f"{row['percent_error_avg']} +/- {row['percent_error_std']} ({row['percent_error_max']})",
+        axis=1,
+    )
+
+    # Separate the camel case word in the Model column
+    metrics_df["Model"] = metrics_df["Model"].str.replace(r"(?<!^)(?=[A-Z])", " ")
+
+    # Update the column names
+    metrics_df = metrics_df.rename(
+        columns={
+            "inter_avg": "Interpolation %",
+            "extra_avg": "Extrapolation %",
+            "dX_sum": "d$X$ Sum",
+            "t_sum": "Time [s]",
+            "new_col": "Planes %",
+        },
+    )
+
+    # drop the columns we don't want
+    metrics_df = metrics_df.drop(
+        columns=[
+            "percent_error_std",
+            "percent_error_max",
+            "high_error_pixel",
+        ],
+    )
+
+    # make Model the index
+    metrics_df = metrics_df.set_index("Model")
+
+    # Reorder the columns
+    metrics_df = metrics_df.reindex(
+        columns=[
+            "Planes %",
+            "Interpolation %",
+            "Extrapolation %",
+            "d$X$ Sum",
+            "Time [s]",
+        ],
+    )
+
+    # Make a latex table
+    latex_table = metrics_df.to_latex(
+        index=True,
+        float_format="%.2f",
+        column_format="l" + "c" * (len(metrics_df.columns) - 1),
+        escape=False,
+    )
+
+    print(latex_table)
+
+
+if __name__ == "__main__":
+    main()
