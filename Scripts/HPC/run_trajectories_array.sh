7921cae1c6aebcdcff906054fcfc514583c2c2b6
John M <john.r.martin@colorado.edu>
2023-06-11 14:01:56 -0600

    HPC traj

---
+++ Scripts/HPC/run_trajectories_array.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1G
+#SBATCH --array=0-100
+#SBATCH --time=00:15:00
+#SBATCH --partition=amilan
+#SBATCH --output=SlurmFiles/trajectory-%j-%a.out
+
+module purge
+
+echo "== Load Anaconda =="
+
+module load anaconda
+
+echo "== Activate Env =="
+
+conda activate research
+
+# Installed package in the compile node already
+# echo "== Install =="
+# cd /projects/joma5012/GravNN/
+# conda develop .
+
+echo "== Run data generation =="
+
+# Note the --exclusive flag ensures that only the one script can be run on the node at a time. 
+echo "Running hparams"
+
+ #$SLURM_ARRAY_TASK_ID
+srun python /projects/joma5012/StatOD/Scripts/DataGeneration/Trajectories/generate_asteroid_rotating_traj_HPC.py $SLURM_ARRAY_TASK_ID
+
+wait # Necessary to wait for all processes to finish
+echo "== End of Job =="
+exit 0
